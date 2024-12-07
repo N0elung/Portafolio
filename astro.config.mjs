06b32574e8d32620c091fcc204530ec140a3a2a7
N0elung <noelb.davalos@gmail.com>
2024-12-07 12:35:19 -0800

    Version 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 // @ts-ignore
 import astroI18next from "astro-i18next";
 
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroI18next()]
+  site: "http://localhost:4321/",
+  integrations: [tailwind(), react(), astroI18next(), sitemap(), robotsTxt()],
 });
